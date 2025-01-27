b48a31b44c3d8936e8d708783f89fca916afa82d
Syafrizal <syafrizalizal17@gmail.com>
2025-01-27 23:18:12 +0700

    Fix Post List and Post Card
    - Make post card
    - Fix Mobile visibility
    - Fix Post List

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,13 @@
 import { Link } from 'react-router-dom';
+import classes from './Post.module.css';
 
-import classes from './Post.module.css'
-
-function Post({ id, author, body }) { 
+function Post({ id, author, body }) {
   return (
     <li className={classes.post}>
-        <Link to={id}> 
-          <p className={classes.author}>{author}</p>
-          <p className={classes.text}>{body}</p>
-        </Link>
+      <Link to={id}>
+        <p className={classes.author}>{author}</p>
+        <p className={classes.text}>{body}</p>
+      </Link>
     </li>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,27 +1,25 @@
 import { useLoaderData } from "react-router-dom";
-
 import Post from "./Post";
 import classes from './PostList.module.css';
 
-function PostList({ }) { 
+function PostList() { 
     const posts = useLoaderData();
 
-  
+    if (posts.length === 0) {
+        return (
+            <div className="no-posts" style={{ textAlign: 'center', color: 'white' }}>
+                <h2>There are no posts yet.</h2>
+                <p>Start adding some</p>
+            </div>
+        );
+    }
+
     return (
-        <>
-            {posts.length > 0 && (
-                <ul className={classes.posts}>
-                {posts.map((post) => <Post key={post.body} id={post.id} author={post.author} body={post.body}/>)}
-            </ul>
-            )}
-            {posts.length === 0 && (
-                <div style={{ textAlign: 'center', color: 'white'}}>
-                    <h2>There are no posts yet.</h2>
-                    <p>Start adding some</p>
-                </div>
-            )}
-            
-        </>
+        <ul className={classes.posts}>
+            {posts.map((post) => (
+                <Post key={post.id} id={post.id} author={post.author} body={post.body} />
+            ))}
+        </ul>
     );
 }
 
