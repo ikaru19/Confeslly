be5b8f0bd1bf832708703bfd6fcbf97999d7fbac
Syafrizal <syafrizalizal17@gmail.com>
2025-01-27 22:52:01 +0700

    Fixing New Post
    - Better UI
    - Support Mobile
    - Add Animation
    - Add Font

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -5,42 +5,54 @@ import classes from './NewPost.module.css';
 import Modal from '../components/Modal';
 
 function NewPost({ onAddPost }) {
-    return (
-        <Modal>
-            <Form method='post' className={classes.form}>
-            <p>
-                <label htmlFor="name">Someone you want to whisper to</label>
-                <input type="text" name='author' id="name" required placeholder="Enter the person's name here"/>
-            </p>
-            <p>
-                <label htmlFor="body">Text</label>
-                <textarea id="body" name='body' required rows={3} placeholder="Write your whispered message..."/>
-            </p>
-            <p className={classes.actions}>
-                <Link to=".." type='button'>
-                    Cancel
-                </Link>
-                <button type="submit">
-                <FaRegPaperPlane /> Submit
-                </button>
-            </p>
-            </Form>
-        </Modal>
-    );
+  return (
+    <Modal>
+      <Form method='post' className={classes.form}>
+        <p>
+          <label htmlFor="name">Someone you want to whisper to</label>
+          <input 
+            type="text" 
+            name='author' 
+            id="name" 
+            required 
+            placeholder="Enter the person's name here" 
+          />
+        </p>
+        <p>
+          <label htmlFor="body">Text</label>
+          <textarea 
+            id="body" 
+            name='body' 
+            required 
+            rows={5} 
+            placeholder="Write your whispered message..." 
+          />
+        </p>
+        <p className={classes.actions}>
+          <Link to=".." type='button' className={classes.cancelButton}>
+            Cancel
+          </Link>
+          <button type="submit" className={classes.submitButton}>
+            <FaRegPaperPlane /> Submit
+          </button>
+        </p>
+      </Form>
+    </Modal>
+  );
 }
 
 export default NewPost;
 
 export async function action({request}) {
-    const formData = await request.formData();
-    const postData = Object.fromEntries(formData)    
-    await fetch('http://localhost:4000/posts', {
-        method: 'POST',
-        body: JSON.stringify(postData),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+  const formData = await request.formData();
+  const postData = Object.fromEntries(formData);
+  await fetch('http://localhost:4000/posts', {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
 
-    return redirect('/');
+  return redirect('/');
 }
